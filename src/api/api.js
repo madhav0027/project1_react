8c0d3fa420f90f3d2edfd13ec0e0f2d9153cd350
Madhav <ashabobby73@gmail.com>
2024-09-06 13:37:34 +0530

    updat 3

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
     baseURL:'https://480b-2409-40d0-301d-741d-8c61-5b97-582b-a988.ngrok-free.app',
-    withCredentials:true
+    // withCredentials:true
 })
 
